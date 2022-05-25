9fa2114de136bca3123f554233a639a684e0f6bc
v_pothurajula <v_pothurajula@dell.com>
2022-05-25 05:36:33 -0700

    First review Commit

--- apicall.py
+++ apicall.py
@@ -1,20 +1,26 @@
 import requests
 
 def getParentid(build_num):
-    url = "http://ibid.usd.lab.emc.com/api/search?build_revision=%22" + build_num + "%22"
-    response = requests.get(url)
-    print (type(response.status_code))
 
-    if (response.status_code == 200):
-        url = ''.join((response.json()['results']))
-        response2 = requests.get(url)
-        parent_id = ''.join((response2.json()['parent_transaction_id']))
-        if (parent_id):
-            return parent_id
+    try:
+        url = "http://ibid.usd.lab.emc.com/api/search?build_revision=%22" + build_num + "%22"
+        response = requests.get(url)
+
+        if (response.status_code == 200):
+            url = ''.join((response.json()['results']))
+            response2 = requests.get(url)
+            parent_id = ''.join((response2.json()['parent_transaction_id']))
+            if (parent_id):
+                return parent_id
+            else:
+                print ("No parent id found. Stopping the script")
+                exit(-1)
         else:
-            print ("No parent id found. Stopping the script")
-            exit()
-    else:
-        print ("unable to reach ",url)
-        print ("Hence exiting the script")
-        exit()
+            print ("unable to reach ",url)
+            print ("Hence exiting the script")
+            exit(-1)
+
+    except Exception:
+
+        print ("Unable to get Parent Transaction ID. PLease check build number provided or build url once")
+        exit(-1)
---
+++ test.py
@@ -0,0 +1,18 @@
+# sles12='10.207.128.32'
+# sles15='10.244.32.23'
+# build_num='5.1.3.0.5.003'
+import shutil
+shutil.copy("env.config","env2.config")
+
+
+
+# with open("env2.config","rt") as fd:
+#     data=fd.read()
+#     data=data.replace('<user>','v_pothurajula')
+#
+# print ("***************************************************")
+# print (data)
+#
+# with open("env2.config","wt") as fd:
+#     fd.write(data)
+
